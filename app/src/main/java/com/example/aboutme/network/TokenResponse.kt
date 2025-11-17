58d90c802b1d340e8bf7dff71a6a4df1e12c802a
KuriZd <KuriZd@protonmail.com>
2025-11-17 04:14:41 -0600

    feat(spotify): modelar respuesta de token

--- app/src/main/java/com/example/aboutme/network/TokenResponse.kt
+++ app/src/main/java/com/example/aboutme/network/TokenResponse.kt
@@ -1,2 +1,11 @@
 package com.example.aboutme.network
 
+import com.squareup.moshi.Json
+
+data class TokenResponse(
+    @Json(name = "access_token") val accessToken: String,
+    @Json(name = "token_type") val tokenType: String,
+    @Json(name = "expires_in") val expiresIn: Long,
+    @Json(name = "refresh_token") val refreshToken: String?,
+    val scope: String?
+)
