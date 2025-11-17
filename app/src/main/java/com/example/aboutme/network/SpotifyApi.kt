15caf43c2221156c6acb7a3eacf0ffe3a75d1d54
KuriZd <KuriZd@protonmail.com>
2025-11-17 04:12:50 -0600

    feat(spotify): definir API para intercambio de código por token

--- app/src/main/java/com/example/aboutme/network/SpotifyApi.kt
+++ app/src/main/java/com/example/aboutme/network/SpotifyApi.kt
@@ -1,2 +1,15 @@
 package com.example.aboutme.network
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface SpotifyApi {
+
+    @GET("me/player/currently-playing")
+    suspend fun getCurrentlyPlaying(
+        @Header("Authorization") authHeader: String,
+        @Query("market") market: String? = null // ej. "MX"
+    ): Response<CurrentlyPlayingResponse>
+}
