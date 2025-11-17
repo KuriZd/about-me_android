59b1190ecbf8e6814ecec8e54189f74714c5daa5
KuriZd <KuriZd@protonmail.com>
2025-11-17 04:06:50 -0600

    feat(spotify): manejar estado global de autenticación

--- app/src/main/java/com/example/aboutme/auth/SpotifyAuthState.kt
+++ app/src/main/java/com/example/aboutme/auth/SpotifyAuthState.kt
@@ -1,2 +1,14 @@
-package com.example.aboutme.auth 
+package com.example.aboutme.auth
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+object SpotifyAuthState {
+    var codeVerifier: String? = null
+    var state: String? = null
+
+    var accessToken: String? by mutableStateOf(null)
+
+    var refreshToken: String? = null
+}
