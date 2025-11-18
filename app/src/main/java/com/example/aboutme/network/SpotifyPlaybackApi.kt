edb41053481615b65c63d2ab47d66d684d457c0a
KuriZd <KuriZd@protonmail.com>
2025-11-17 21:59:39 -0600

    fix minor details

--- app/src/main/java/com/example/aboutme/network/SpotifyPlaybackApi.kt
+++ app/src/main/java/com/example/aboutme/network/SpotifyPlaybackApi.kt
@@ -4,11 +4,19 @@ import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface SpotifyPlaybackApi {
 
     @GET("v1/me/player/currently-playing")
     suspend fun getCurrentlyPlaying(
         @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
+
+    @GET("v1/me/top/tracks")
+    suspend fun getTopTracks(
+        @Header("Authorization") authHeader: String,
+        @Query("time_range") timeRange: String = "short_term",
+        @Query("limit") limit: Int = 20
     ): Response<ResponseBody>
 }
