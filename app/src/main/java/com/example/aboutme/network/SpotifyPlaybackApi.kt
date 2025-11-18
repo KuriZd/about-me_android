66eaa4174cb1632ce9f79e51454f884b9d5dcdff
KuriZd <KuriZd@protonmail.com>
2025-11-17 20:43:57 -0600

    cambios para aceptar imagen y duracion

--- app/src/main/java/com/example/aboutme/network/SpotifyPlaybackApi.kt
+++ app/src/main/java/com/example/aboutme/network/SpotifyPlaybackApi.kt
@@ -1,2 +1,14 @@
 package com.example.aboutme.network
 
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface SpotifyPlaybackApi {
+
+    @GET("v1/me/player/currently-playing")
+    suspend fun getCurrentlyPlaying(
+        @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
+}
