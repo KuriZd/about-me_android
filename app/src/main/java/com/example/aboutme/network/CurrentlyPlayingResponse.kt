7f3d7303a9df4f4d623a17dc45ee126ffc778d44
KuriZd <KuriZd@protonmail.com>
2025-11-17 20:43:35 -0600

    cambios para aceptar imagen y duracion

--- app/src/main/java/com/example/aboutme/network/CurrentlyPlayingResponse.kt
+++ app/src/main/java/com/example/aboutme/network/CurrentlyPlayingResponse.kt
@@ -1,17 +1,16 @@
+// network/CurrentlyPlayingResponse
 package com.example.aboutme.network
 
-import com.squareup.moshi.Json
-
 data class CurrentlyPlayingResponse(
-    @Json(name = "is_playing") val isPlaying: Boolean?,
-    @Json(name = "progress_ms") val progressMs: Int?,
+    val progressMs: Int?,
     val item: SpotifyTrack?
 )
 
 data class SpotifyTrack(
     val name: String?,
     val artists: List<SpotifyArtist>?,
-    @Json(name = "duration_ms") val durationMs: Int?
+    val durationMs: Int?,
+    val imageUrl: String?
 )
 
 data class SpotifyArtist(
