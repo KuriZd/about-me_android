129f0e17148ec0c6658ca5beaa93d65385d440f5
KuriZd <KuriZd@protonmail.com>
2025-11-17 21:59:00 -0600

    fix minor details

--- app/src/main/java/com/example/aboutme/auth/SpotifyAuthConfig.kt
+++ app/src/main/java/com/example/aboutme/auth/SpotifyAuthConfig.kt
@@ -1,11 +1,12 @@
 package com.example.aboutme.auth
 
-const val SPOTIFY_CLIENT_ID = "89e02f14cb9d4163b6315b334bfc4a68" // cámbialo
+const val SPOTIFY_CLIENT_ID = "89e02f14cb9d4163b6315b334bfc4a68"
 const val SPOTIFY_REDIRECT_URI = "kuri-spotify-login://callback"
 
 const val SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 
 val SPOTIFY_SCOPES = listOf(
     "user-read-currently-playing",
-    "user-read-playback-state"
+    "user-read-playback-state",
+    "user-top-read"
 )
